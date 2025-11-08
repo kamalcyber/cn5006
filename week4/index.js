7176f7d4c6c7258ebd78bba7780229534929cc2a
kamalcyber <kamalsah934@gmail.com>
2025-11-08 03:20:07 +0000

    Add files via upload

--- week4/index.js
+++ week4/index.js
@@ -1,1 +1,28 @@
 
+var express=require("express")
+var fs= require("fs")
+var app=express()
+// add middle ware function for body parsing
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/',function(req,res){
+res.send("hello it is my first express application")
+})
+
+
+app.get('/about',function(req,res)
+{ res.send("This is basic express application ")
+})
+app.get('/users/:userId/books/:bookId', function (req, res) { res.send(req.params)
+})
+app.listen(5000,function(){console.log("server is running on port 5000")})
+
+app.get('/GetStudents',function (req,res)
+ { studentdata={}
+ fs.readFile(__dirname + "/" + "Student.json", 'utf8', function (err,
+data) { console.log( data );
+ res.json({ 'status':true, 'Status_Code':200,
+ 'requested at': req.localtime, 'requrl':req.url,
+ 'request Method':req.method, 'studentdata':JSON.parse(data)});
+});
+}) 
